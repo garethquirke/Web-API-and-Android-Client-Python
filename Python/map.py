ca0412e8dd85c82f639a6602a343087918b85688
garethquirke <gareth.quirke@gmail.com>
2017-05-02 00:18:02 +0100

    additions

---
+++ Python/map.py
@@ -0,0 +1,24 @@
+# map functions allow us to exectute a function on each item of a list
+
+# simple 1 to ten list and outputted in for loop
+onetoten = list(range(1,11))
+print(onetoten)
+
+def doublenum(num):
+    return num *2
+
+# creating a new list, mapping a function to it and a original list(onetoten)
+multiplesoftwo = list(map(doublenum, onetoten))
+print(multiplesoftwo)
+
+# now we will square each element by itself
+# to create a list of powers
+def squarenum(num):
+    return num * num
+
+squarelist = list(map(squarenum,onetoten))
+print(squarelist)
+
+# example with lambda as a parameter in the map function
+cubedlist = list(map((lambda x: x**3), onetoten))
+print(cubedlist)
